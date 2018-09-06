7160b05ee85c0d52d858ddf3cdd3b0ab469060f2
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-06 17:33:04 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def word_substituter(string)
+  string.split(" ").each do |word|
+    case word
+    when "hello"
+      'hi'
+    when ("to"|| "two"||"too")
+      '2'
+    when "for"||"four"
+      '4'
+    when "be"
+      "b"
+    when "you"
+      'u'
+    when "at"
+      "@"
+    when "and"
+        "&"
+    end.join(" ")
+end
